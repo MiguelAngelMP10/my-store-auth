c564ee27137d4284e52bcca83e82ce6ae7c49315
Miguel Angel Muñoz Pozos <miguel.pozos@wagento.com>
2022-02-28 19:25:25 -0600

    Implementando hashing para usuarios

--- services/user.service.js
+++ services/user.service.js
@@ -1,12 +1,19 @@
 const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
+
 
 const { models } = require('./../libs/sequelize');
 
 class UserService {
-  constructor() {}
+  constructor() { }
 
   async create(data) {
-    const newUser = await models.User.create(data);
+    const hash = await bcrypt.hash(data.password, 10);
+    const newUser = await models.User.create({
+      ...data,
+      password: hash
+    });
+    delete newUser.dataValues.password;
     return newUser;
   }
 
